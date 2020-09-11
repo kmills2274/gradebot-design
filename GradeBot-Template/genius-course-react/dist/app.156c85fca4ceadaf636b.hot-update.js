613c0b746a634c45ca780d426ce9a895bcfe0378
kate.m <katiemills0810@gmail.com>
2020-09-10 23:37:12 -0500

    template files added, logo added to navbar, background image changed

---
+++ GradeBot-Template/genius-course-react/dist/app.156c85fca4ceadaf636b.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate("app",{
+
+/***/ "./src/assets/scss/style.scss":
+/*!************************************!*\
+  !*** ./src/assets/scss/style.scss ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\nModuleBuildError: Module build failed (from ./node_modules/image-webpack-loader/index.js):\\nError: ENOENT: no such file or directory, open '/Users/kmills/Desktop/Genius_Course_React/genius-course-react/src/assets/img/banner/learning.jpg'\\n    at runLoaders (/Users/kmills/Desktop/Genius_Course_React/genius-course-react/node_modules/webpack/lib/NormalModule.js:316:20)\\n    at /Users/kmills/Desktop/Genius_Course_React/genius-course-react/node_modules/loader-runner/lib/LoaderRunner.js:367:11\\n    at /Users/kmills/Desktop/Genius_Course_React/genius-course-react/node_modules/loader-runner/lib/LoaderRunner.js:203:19\\n    at process.nextTick (/Users/kmills/Desktop/Genius_Course_React/genius-course-react/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:85:15)\\n    at process._tickCallback (internal/process/next_tick.js:61:11)\");\n\n//# sourceURL=webpack:///./src/assets/scss/style.scss?");
+
+/***/ })
+
+})
